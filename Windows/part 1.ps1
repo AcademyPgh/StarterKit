7b692d85689e36213847c781a3de461b551ce2ac
Jean Lange <jean.f.lange@gmail.com>
2019-04-23 12:28:04 -0400

    Update part 1.ps1

--- Windows/part 1.ps1
+++ Windows/part 1.ps1
@@ -9,7 +9,7 @@ cinst putty -y
 cinst python2 -y
 cinst git -y
 cinst visualstudiocode -y
-cinst visualstudio2019community --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
+cinst visualstudio2019community -y --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
 
 # For Rails Installers
 cinst ruby --version=2.3.3 -y
