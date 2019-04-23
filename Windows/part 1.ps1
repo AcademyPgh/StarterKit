67246dc2b833cc453951f7781be6070fbe376f38
Jean Lange <jean.f.lange@gmail.com>
2019-04-23 13:21:32 -0400

    Update part 1.ps1

--- Windows/part 1.ps1
+++ Windows/part 1.ps1
@@ -12,9 +12,8 @@ cinst visualstudiocode -y
 cinst visualstudio2019community -y --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
 
 # For Rails Installers
-cinst ruby --version=2.3.3 -y
-cinst msys2 -y
-cinst ruby2.devkit -y
+cinst ruby -y
+cinst msys2 --params "/NoUpdate" -y
 cinst sqlite -y
 cinst sqlite.shell -y
 cinst nodejs.install -y
