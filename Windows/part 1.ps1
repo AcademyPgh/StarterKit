37d426480ecb41cd8c7ffb527e930acc41def027
Jean Lange <jean.f.lange@gmail.com>
2019-04-23 14:51:54 -0400

    Update part 1.ps1

--- Windows/part 1.ps1
+++ Windows/part 1.ps1
@@ -8,6 +8,7 @@ cinst javaruntime -y
 cinst putty -y
 cinst python2 -y
 cinst git -y
+cinst riot-web -y
 cinst visualstudiocode -y
 cinst visualstudio2019community -y --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
 
