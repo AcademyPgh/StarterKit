2318135f53b3f386eeeebd8b047c9a68b20e74e5
John Lange <lange.john.m@gmail.com>
2021-07-19 12:25:03 -0400

    Update part 2.ps1
    Fixed a missed command on the build tools.

--- Windows/part 2.ps1
+++ Windows/part 2.ps1
@@ -1,9 +1,9 @@
-# ridk install 2 3
 #cinst visualstudio2017buildtools --package-parameters "--allWorkloads" -y
 #npm config set msbuild_path "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
 
 ridk exec pacman -Syu
 ridk exec pacman -S mingw-w64-x86_64-dlfcn
+ridk install 2 3
 cinst ruby --version=2.6.5.1 --side-by-side -y
 ridk use 1
 gem install sqlite3
