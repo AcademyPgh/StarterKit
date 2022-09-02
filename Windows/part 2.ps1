aa221a6c096f6a7e9434d9857f2990925ec38d44
John Lange <lange.john.m@gmail.com>
2022-09-02 15:14:53 -0400

    Remove extra items

--- Windows/part 2.ps1
+++ Windows/part 2.ps1
@@ -1,12 +1,3 @@
-#cinst visualstudio2017buildtools --package-parameters "--allWorkloads" -y
-#npm config set msbuild_path "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
-
-ridk exec pacman -Syu
-ridk exec pacman -S mingw-w64-x86_64-dlfcn
-# select "all"
-ridk exec pacman -S mingw-w64-x86_64-toolchain
-ridk install 2 3
-# cinst ruby --version=2.6.5.1 --side-by-side -y
-# ridk use 1
+ridk install 3
 gem install sqlite3
 gem install rails
