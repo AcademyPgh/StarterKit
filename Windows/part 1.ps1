0f2db046943de1b9e02a489d7d0657652f65d2fc
Jean Lange <jean.f.lange@gmail.com>
2024-02-08 15:40:46 -0500

    Update part 1.ps1 for 2024
    mostly:
    - remove riot, replace with mattermost
    - update vscode package name
    - replace cinst with choco install

--- Windows/part 1.ps1
+++ Windows/part 1.ps1
@@ -2,30 +2,30 @@
 Set-ExecutionPolicy RemoteSigned -Force
 
 # General Installers
-cinst googlechrome -y
-cinst firefox -y
-cinst 7zip -y
-cinst openjdk --version=15.0.2 -y
-cinst putty -y
-cinst python2 -y
-cinst git -y
-cinst dbeaver -y
-cinst riot-web -y
-cinst visualstudiocode -y
-cinst visualstudio2022community -y --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
-cinst zoom -y
-cinst dotnetcore-sdk -y
-cinst microsoft-windows-terminal -y
+choco install googlechrome -y
+choco install firefox -y
+choco install 7zip -y
+choco install openjdk --version=15.0.2 -y
+choco install putty -y
+choco install python3 -y
+choco install git -y
+choco install dbeaver -y
+choco install mattermost-desktop -y
+choco install vscode -y
+choco install visualstudio2022community -y --package-parameters "--addProductLang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Python --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional --passive"
+choco install zoom -y
+choco install dotnetcore-sdk -y
+choco install microsoft-windows-terminal -y
 
 # For Rails Installers
-cinst ruby -y
-cinst sqlite -y
-cinst sqlite.shell -y
-cinst nodejs-lts -y
-cinst yarn -y
+choco install ruby -y
+choco install sqlite -y
+choco install sqlite.shell -y
+choco install nodejs-lts -y
+choco install yarn -y
 
 # Run part 2 in a separate shell to reload ruby and all the things
-cinst msys2 -y
+choco install msys2 -y
 
 # New ruby installer breaks permissions for non-admins to add gems, fix it
 $rubypath = 'C:\tools\ruby31\'
@@ -33,4 +33,4 @@ $perms = Get-Acl -Path $rubypath
 $perms.SetAccessRuleProtection($False,$true)
 Set-Acl -Path $rubypath -AclObject $perms
 
-# extra comment line just to make sure that that cinst msys2 runs for real!
+# extra comment line just to make sure that that choco install msys2 runs for real!
